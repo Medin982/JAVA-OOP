1e561a6698d6dc83f73ba4a1a7c402594330db86
Medin Tarakchiev <warss982@gmail.com>
2021-11-04 15:12:35 +0200

    Encapsulation

--- src/WorkingwithAbstraction/Exercise/JediGalaxy/greedyTimes/Bags.java
+++ src/WorkingwithAbstraction/Exercise/JediGalaxy/greedyTimes/Bags.java
@@ -1,21 +1,20 @@
-//package WorkingwithAbstraction.Exercise.JediGalaxy.greedyTimes;
-//
-//import java.util.Collection;
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//
-//public class Bags {
-//    private int capacity;
-//    private Map<String, Map<String, Integer>> date;
-//
-//    public Bags(int capacity) {
-//        this.capacity = capacity;
-//        this.date = new LinkedHashMap<>();
-//    }
-//
-//    public boolean bagsCapacity(int quantity) {
-//        return this.capacity < this.date.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + quantity;
-//    }
-//
-//    public void
-//}
+package WorkingwithAbstraction.Exercise.JediGalaxy.greedyTimes;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Bags {
+    private int capacity;
+    private Map<String, Integer> date;
+
+    public Bags(int capacity) {
+        this.capacity = capacity;
+        this.date = new LinkedHashMap<>();
+    }
+
+    public boolean bagsCapacity(int quantity) {
+        return this.capacity < this.date.values().stream().mapToLong(e -> e).sum() + quantity;
+    }
+
+}
